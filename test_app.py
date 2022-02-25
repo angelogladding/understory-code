60fc7bd343786dcd044cbb15e3a3c967a5d35781
Angelo Gladding <angelo@lahacker.net>
2022-02-25 10:42:31 -0800

    Clean up tests

--- test_app.py
+++ test_app.py
@@ -1,13 +1,16 @@
-import pathlib
+"""Test understory-code."""
+
+from pathlib import Path
 
 import understory.code
 from understory.code import app
 
-understory.code.package_dir = pathlib.Path("test_packages")
-understory.code.project_dir = pathlib.Path("test_projects")
+understory.code.package_dir = Path("test_packages")
+understory.code.project_dir = Path("test_projects")
 
 
 def test_index():
+    """Test the main code index."""
     response = app.get("")
     assert response.status == "200 OK"
     assert len(response.dom.select("li")) == 0
